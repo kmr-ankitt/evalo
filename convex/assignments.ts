11c85c96aaf5226783ce040697cc0815490fd8ce
kishu279 <souravpoddar93042@gmail.com>
2025-11-08 11:15:30 +0530

    assignment.ts changed again

--- convex/assignments.ts
+++ convex/assignments.ts
@@ -12,6 +12,7 @@ export const newAssignments = mutation({
     createdAt: v.number(),
     testCase: v.string(),
     dueDate: v.number(),
+    compileTime: v.number(),
   },
   handler: async (ctx, args) => {
     // before creating the assignment check if the teacher exists
@@ -21,61 +22,7 @@ export const newAssignments = mutation({
       return { success: false, error: "Teacher not found" };
     }
 
-    let startTime = Date.now();
-    let endTime = 0;
-
     try {
-      const runtime = LANGUAGE_CONFIG[args.language].pistonRuntime;
-      const response = await fetch(`https://emkc.org/api/v2/piston/execute`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          language: runtime.language,
-          version: runtime.version,
-          files: [{ content: args.solution }],
-          args: args.testCase.split('\n').map(testCase => testCase.trim()),
-        }),
-      });
-
-      const data = await response.json();
-      console.log("Data back from piston", data);
-
-      // // Handle api level errors
-      // if (data.message) {
-      //   // set({
-      //   //   error: data.message,
-      //   //   executionResult: { code, output: "", error: data.message },
-      //   // });
-      //   return;
-      // }
-
-      // // handle compilation errors
-      // if (data.comile && data.compile.code !== 0) {
-      //   const error = data.compile.stderr || data.compile.output;
-      //   // set({
-      //   //   error,
-      //   //   executionResult: { code, output: "", error },
-      //   // });
-
-      //   return;
-      // }
-
-      // // handle runtime errors
-      // if (data.run && data.run.code !== 0) {
-      //   const error = data.run.stderr || data.run.output;
-      //   // set({
-      //   //   error,
-      //   //   executionResult: { code, output: "", error },
-      //   // });
-
-      //   return;
-      // }
-
-      endTime = Date.now();
-      console.log(`Code executed in ${endTime - startTime} ms`);
-
       const assignmentId = await ctx.db.insert("assignments", {
         teacherId: args.teacherId,
         title: args.title,
@@ -85,7 +32,7 @@ export const newAssignments = mutation({
         createdAt: args.createdAt,
         testCase: args.testCase,
         dueDate: args.dueDate,
-        compileTime: endTime - startTime,
+        compileTime: args.compileTime,
 
         expectedOutput: "", // Placeholder, can be set later
       });
